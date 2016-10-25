5454e2440210754da4797a4feb5f809809464243
Juan Padilla <jxpadilla@yahoo.com>
2016-10-25 14:38:59 -0500

    project setup

---
+++ server.js
@@ -0,0 +1,29 @@
+//Set up middleware:
+
+
+var express = require('express');
+var bodyParser = require('body-parser');
+var path = require('path');
+var app = express();
+var PORT = process.env.PORT || 3000;
+
+//SETS UP DATA PARSING
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.text());
+app.use(bodyParser.json({type:'application/vnd.api+json'}));
+
+var api = require('./app/routing/api-routes.js');
+var html = require('./app/routing/html-routes.js');
+
+//middleware
+app.use('/app', express.static(__dirname + '/public'));
+
+//call route functions
+api(app);
+html(app);
+
+//LISTEN
+app.listen(PORT, function(){
+	console.log('APP LISTENING AT PORT ' + PORT)
+})
